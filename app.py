9f5a4f5d654a4354671baa14951d929d544d3153
Jake Cerwin <jcerwin@andrew.cmu.edu>
2021-05-07 04:13:31 -0400

    Start of gui integration

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jake
+"""
+
 import pandas as pd
 import time
+import tkinter as tk
 
+#import frontend
+from frontend/social_hub_gui import SocialHubApp
+
+
+
+# import backend
 from scrappers.pinterest import PinterestScrapper
 from scrappers.linkedin import LinkedinScrapper
+from scrappers.instagram import InstagramScrapper
+#from scrappers.weheartit
 
 usernamePinterest = 'jake.cerwin@yahoo.com'
 passwordPinterest = 'datafocusedpythOn'
@@ -24,3 +38,22 @@ pinterest.refresh()
 df = pinterest.scrape()
 pinterest.close()
 """
+
+
+
+root = tk.Tk()
+root.title("SocialHub1.0")
+root.geometry('800x500')
+root.resizable(0, 0)
+root.configure(bg='orange')
+
+# create menu
+menu = tk.Menu(root)
+item = tk.Menu(menu)
+item.add_command(label='Exit', command=root.destroy)
+item.add_command(label='Refresh', command=gc.refresh)
+menu.add_cascade(label='File', menu=item)
+
+root.config(menu=menu)
+SocialHubApp(root).pack(side="top", fill="both", expand=True)
+root.mainloop()
--- scrappers/linkedin.py
+++ scrappers/linkedin.py
@@ -5,22 +5,21 @@
 @author: Gini and Jake Cerwin
 """
 
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys 
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-#from requests_html import HTMLSession
 import time
 import pandas as pd
 
 
+debug = True
 
 
 class LinkedinScrapper:
     def __init__(self, username, password):
         options = webdriver.ChromeOptions()
-        #options.headless = True
+        if not debug:
+            options.headless = True
         # instantiate the Chrome class web driver and pass the Chrome Driver Manager
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
@@ -59,8 +58,9 @@ class LinkedinScrapper:
                 c = i.get_text()
                 contents.append({c})
 
-            personal_image = soup.findAll('img',
-                                          attrs={'class': 'presence-entity__image ivm-view-attr__img--centered EntityPhoto-circle-3 feed-shared-actor__avatar-image EntityPhoto-circle-3 lazy-image ember-view'})
+            # Gini pls fix this
+            personal_image = soup.findAll('img', attrs={'class':
+                'presence-entity__image ivm-view-attr__img--centered EntityPhoto-circle-3 feed-shared-actor__avatar-image EntityPhoto-circle-3 lazy-image ember-view'})
             personal_images = []
             names = []
 
@@ -87,7 +87,7 @@ class LinkedinScrapper:
 
         return df
 
-    def close(self ):
+    def close(self):
         self.driver.quit()
 
 
@@ -110,99 +110,6 @@ if __name__ == "__main__":
     linkedin.close()
 
     print(df.head())
-
-
-"""
-# In[107]:
-
-
-from tkinter import simpledialog
-
-ROOT = tk.Tk()
-
-ROOT.withdraw()
-# the input dialog
-USERNAME = simpledialog.askstring(title="Login",
-                                  prompt="Username")
-PASSWORD = simpledialog.askstring(title="Login",
-                                  prompt="Password")
-
-
-# In[108]:
-
-
-#go to LinkedIN's Login page
-self.driver.get('https://www.linkedin.com/login/')
-
-
-# In[109]:
-
-
-# login to account before starting to scrape
-
-username = USERNAME
-password = PASSWORD
-elementID = self.driver.find_element_by_id('username')
-elementID.send_keys(username)
-
-elementID = self.driver.find_element_by_id('password')
-elementID.send_keys(password)
-elementID.submit()
-
-
-# In[125]:
-
-
-#  Get page source code
-
-for _ in range(1,20):
-    src = self.driver.page_source
-    soup = BeautifulSoup(src, "lxml")
-
-    content = soup.findAll('span', attrs = {'class':'break-words'}) + soup.findAll('article', attrs = {'class':'feed-shared-announcement__description-container'})
-    contents = []
-    for i in content:
-        c = i.get_text()
-        print(c)
-        contents.append({c})
-
-    personal_image = soup.findAll('img', attrs={'class': 'presence-entity__image ivm-view-attr__img--centered EntityPhoto-circle-3 feed-shared-actor__avatar-image EntityPhoto-circle-3 lazy-image ember-view'})
-    personal_images = []
-    names = []
-    for i in personal_image:
-    #print(i['src'])
-        d = i['src']
-        e = i['title']
-        personal_images.append({d})
-        names.append({e})
-        
-    number_likes = []
-    for i in soup.findAll('span', attrs={'class': 'v-align-middle social-details-social-counts__reactions-count'}):
-        e = i.get_text()
-        number_likes.append({e})
-    self.driver.execute_script("window.scrollTo(1,100000)")
-    time.sleep(2)
-    
-
-
-# In[126]:
-
-
-#Clean Data
-data = {'User Image url': pd.Series(personal_images),'Name':pd.Series(names), 'Post Content': pd.Series(contents), 'Likes' : pd.Series(number_likes)}
-df = pd.DataFrame(data)
-print(df.head())
-df.dropna(subset = ["User Image url"], inplace=True)
-
-
-# In[100]:
-
-
-df.to_csv('C:/Users/DELL/PycharmProjects/linkedin/Clean_Data.csv', index = False)
-
-
-# In[ ]:
-"""
 
 
 
