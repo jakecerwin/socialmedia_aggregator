7c4dc185a924d70ffc9aba6a1ea773ae1637b8b1
Jake Cerwin <jcerwin@andrew.cmu.edu>
2021-05-10 16:54:56 -0400

    Read entries account page

--- app.py
+++ app.py
@@ -35,68 +35,23 @@ passwordPinterest = 'datafocusedpythOn'
 usernameLinkedIn  = 'jake.cerwin@yahoo.com'
 passwordLinkedIn  = '1800317'
 
-if not static:
-    instagram_followers = ['carnegiemellon', 'iris_rover', 'mse_cmu', 'tartanathletics', 'cmusasc']
-    weheartit_searches = ['tech', 'travel', 'plants', 'design']
+# move example_data to data
 
 
-    # create scrappers
-    linkedin = LinkedinScrapper(usernameLinkedIn, passwordLinkedIn)
-    pinterest = PinterestScrapper(usernamePinterest, passwordPinterest)
-    instagram = InstagramScrapper(instagram_followers)
-    weheartit = WeheartitScrapper(weheartit_searches)
-    scrappers = [linkedin, pinterest, instagram, weheartit]
-    scrapper_labels = ['linkedin', 'pinterest', 'instagram', 'weheartit']
+root = tk.Tk()
+root.title("SocialHub1.0")
+root.geometry('800x500')
+root.resizable(0, 0)
+root.configure(bg='orange')
 
+# create menu
+menu = tk.Menu(root)
+item = tk.Menu(menu)
+item.add_command(label='Exit', command=root.destroy)
+item.add_command(label='Refresh', command=gc.refresh)
+menu.add_cascade(label='File', menu=item)
 
-    # scrape
-    dfs = [pd.DataFrame(df_labels)] * len(scrappers)
-    for i in range(len(scrappers)):
-
-        try:
-            df = scrappers[i].scrape()
-            if df is not None:
-                dfs[i] = df
-        except:
-            print("failure: " + scrapper_labels[i] )
-
-
-
-
-
-    # save
-    for i in range(len(scrappers)):
-        dfs[i].to_csv('data/'+str(scrapper_labels[i])+'.csv', index=False)
-
-
-    linkedin.close()
-    pinterest.close()
-    weheartit.close()
-    instagram.close()
-
-"""
-pinterest = PinterestScrapper(username, password)
-df = pinterest.scrape()
-pinterest.refresh()
-df = pinterest.scrape()
-pinterest.close()
-"""
-
-if static:
-    root = tk.Tk()
-    root.title("SocialHub1.0")
-    root.geometry('800x500')
-    root.resizable(0, 0)
-    root.configure(bg='orange')
-
-    # create menu
-    menu = tk.Menu(root)
-    item = tk.Menu(menu)
-    item.add_command(label='Exit', command=root.destroy)
-    item.add_command(label='Refresh', command=gc.refresh)
-    menu.add_cascade(label='File', menu=item)
-
-    root.config(menu=menu)
-    SocialHubApp(root).pack(side="top", fill="both", expand=True)
-    root.mainloop()
+root.config(menu=menu)
+SocialHubApp(root).pack(side="top", fill="both", expand=True)
+root.mainloop()
 
--- frontend/account_page_gui.py
+++ frontend/account_page_gui.py
@@ -23,13 +23,57 @@ class AccountPage(tk.Frame):
 
 class AccountFrame(tk.Frame):
     def __init__(self, parent, controller):
-        self.u_name = tk.StringVar()
-        self.pw = tk.StringVar()
+
+        accounts = pd.read_csv('user_data/accounts.csv')
+        following = pd.read_csv('user_data/instagram_follows.csv')
+        topics = pd.read_csv('user_data/weheartit_topics.csv')
+        followingstr,topicstr = '', ''
+
+        for i, row in following.iterrows():
+            #print(row.to_numpy()[0])
+            followingstr += row.to_numpy()[0] + ', '
+
+        for i, row in topics.iterrows():
+            #print(row.to_numpy()[0])
+            topicstr += row.to_numpy()[0] + ', '
+
+
+        pinterest = accounts.loc[accounts['media'] == 'pinterest']
+        linkedin = accounts.loc[accounts['media'] == 'linkedin']
+
+        self.pi_acc = tk.StringVar(value=pinterest['username'].to_numpy()[0])
+        self.pi_pas = tk.StringVar(value=pinterest['password'].to_numpy()[0])
+        self.lk_acc = tk.StringVar(value=linkedin['username'].to_numpy()[0])
+        self.lk_pas = tk.StringVar(value=linkedin['password'].to_numpy()[0])
+
+
+
+        self.ig = tk.StringVar(value=followingstr)
+        self.wh = tk.StringVar(value=topicstr)
 
         tk.Frame.__init__(self, parent, width=600, height=400, bg='#3e433f')
-        #tk.Label(self, text='Welcome to socialHub', fg='white', bg='#3e433f').grid(row=1, column=1, sticky='n')
-        tk.Label(self, text='Instagram Password:', fg='white', bg='#3e433f').grid(row=2, column=0)
-        self.user_name = tk.Entry(self, textvariable=self.u_name).grid(row=1, column=1)
+
+        tk.Label(self, text='Pinterest Account:', fg='white', bg='#3e433f').grid(row=0, column=0)
+        tk.Entry(self, textvariable=self.pi_acc).grid(row=0, column=1)
+        tk.Label(self, text='Pinterest Password:', fg='white', bg='#3e433f').grid(row=1, column=0)
+        tk.Entry(self, textvariable=self.pi_pas).grid(row=1, column=1)
+        tk.Label(self, text='Linkedin Account:', fg='white', bg='#3e433f').grid(row=2, column=0)
+        tk.Entry(self, textvariable=self.lk_acc).grid(row=2, column=1)
+        tk.Label(self, text='Linkedin Password:', fg='white', bg='#3e433f').grid(row=3, column=0)
+        tk.Entry(self, textvariable=self.lk_pas).grid(row=3, column=1)
+
+        tk.Label(self, text='For the following seperate ',
+                 fg='white', bg='#3e433f').grid(row=4, column=0, sticky='e')
+        tk.Label(self, text='search terms by commas',
+                 fg='white', bg='#3e433f').grid(row=4, column=1, sticky='w')
+
+        tk.Label(self, text='WeHeartIt Topics:', fg='white', bg='#3e433f').grid(row=5, column=0)
+        tk.Entry(self, textvariable=self.ig).grid(row=5, column=1)
+        tk.Label(self, text='Instagram Accounts to follow:', fg='white', bg='#3e433f').grid(row=6, column=0)
+        tk.Entry(self, textvariable=self.wh).grid(row=6, column=1)
+
+
+
 
         """
         tk.Label(self, text='Name', fg='white', bg='#3e433f').grid(row=1, column=0, sticky='w')
@@ -37,15 +81,19 @@ class AccountFrame(tk.Frame):
         tk.Label(self, text='Password', fg='white', bg='#3e433f').grid(row=2, column=0, sticky='w')
         self.password = tk.Entry(self, textvariable=self.pw).grid(row=2, column=1)
         """
-        tk.Label(self, text='click when ready', fg='white', bg='#3e433f').grid(row=3, column=0)
-        tk.Button(self, text='submit', command=lambda: self.submit(controller, self.static)).grid(row=3, column=1)
+        tk.Label(self, text='click when ready', fg='white', bg='#3e433f').grid(row=7, column=0)
+        tk.Button(self, text='submit', command=lambda: self.submit(controller)).grid(row=7, column=1)
 
-    def submit(self, controller, static):
+
+    def submit(self, controller):
 
         #gc.log_in(self.u_name.get(), self.pw.get())
+        print([self.pi_acc.get(), self.pi_pas.get(), self.lk_acc.get(),
+               self.lk_pas.get(), self.ig.get(), self.wh.get()])
+        breakpoint()
 
         print('scraping')
-        scrape()
+        #scrape()
 
         controller.load_main() # change to dynamic
 
